1984bfd80f5aa42d14d11af7913da4938624611e
Mike Kelly <mike@kellyfam.net>
2018-08-02 08:29:57 -0400

    add addresses to card

--- src/components/FamilyCard.js
+++ src/components/FamilyCard.js
@@ -16,8 +16,50 @@ export default class FamilyCard extends Component {
             {family.firstName} {family.lastName} <br />
             <small>{family.children}</small>
           </h5>
+          <div className="card-text">
+            <FamilyAddress family={family} />
+          </div>
         </div>
       </div>
+    );
+  }
+}
+
+class FamilyAddress extends Component {
+  static propTypes = {
+    family: PropTypes.object
+  };
+  render() {
+    var family = this.props.family;
+
+    return (
+      <address>
+        {family.street}
+        {family.street && <br />}
+        {family.city && `${family.city},`} {family.state} {family.zip}
+        {(family.city || family.state || family.zip) && <br />}
+        {family.street &&
+          family.city && (
+            <a
+              target="_blank"
+              href={`https://www.google.com/maps/search/?api=1&query=${encodeURI(
+                `${family.street} ${family.city} ${family.state} ${family.zip}`
+              )}`}
+              className="map-link"
+            >
+              Map
+            </a>
+          )}
+        {family.street && family.city && <br />}
+        {family.phone && (
+          <span>
+            <abbr title="Phone">P:</abbr>{" "}
+            <a href={"tel:" + family.phone}>{family.phone}</a> <br />
+          </span>
+        )}
+        <abbr title="Email">E:</abbr>{" "}
+        <a href={"mailto:" + family.email}>{family.email}</a> <br />
+      </address>
     );
   }
 }
